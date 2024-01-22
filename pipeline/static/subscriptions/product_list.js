6a21953f994128b64df859aa4c362f4c29e31ead
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2024-01-22 16:28:09 +0000

    Added delete all products button for user subscriptions

--- pipeline/static/subscriptions/product_list.js
+++ pipeline/static/subscriptions/product_list.js
@@ -18,3 +18,26 @@ function getUserEmail() {
                 xhr_request.send("product_name=" + encodeURIComponent(productName) + "&user_email=" + encodeURIComponent(userEmail));
             }
         }
+        function deleteAllItems(element, productName) {
+            var userEmail = getUserEmail()
+            var xhr_request = new XMLHttpRequest();
+            xhr_request.open("POST", "/delete_subscription", true);
+            xhr_request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr_request.onreadystatechange = function() {
+                if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                    element.parentNode.parentNode.remove();
+                    window.location.reload()
+                }
+            }
+            xhr_request.send("product_name=" + encodeURIComponent(productName) + "&user_email=" + encodeURIComponent(userEmail));
+        }
+function deleteAllProducts() {
+    if (confirm("Are you sure you want to unsubscribe from all products?")) {
+        var table = document.querySelector('table tbody');
+        Array.from(table.rows).forEach(row => {
+            // Assuming the product name is in the second cell of each row
+            var productName = row.cells[1].textContent;
+            deleteAllItems(row.cells[0].getElementsByTagName('button')[0], productName);
+        });
+    }
+}
